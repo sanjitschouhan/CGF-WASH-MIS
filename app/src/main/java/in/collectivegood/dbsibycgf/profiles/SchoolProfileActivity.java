05e833f408d7e0846ff82802d56eaefc11137ae4
sanjit <sanjitschouhan@gmail.com>
2017-06-05 11:43:51 +0530

    Packages

---
+++ app/src/main/java/in/collectivegood/dbsibycgf/profiles/SchoolProfileActivity.java
@@ -0,0 +1,15 @@
+package in.collectivegood.dbsibycgf.profiles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.collectivegood.dbsibycgf.R;
+
+public class SchoolProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school);
+    }
+}
