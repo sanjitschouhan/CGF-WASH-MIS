7a6867867d6d33875181b19ab199da497a04078e
PattanAsifKhan <aisf.khan.399@gmail.com>
2017-06-05 12:51:50 +0530

    calender left for sanjit

--- app/src/main/java/in/collectivegood/dbsibycgf/calender/CalendarActivity.java
+++ app/src/main/java/in/collectivegood/dbsibycgf/calender/CalendarActivity.java
@@ -1,15 +1,28 @@
 package in.collectivegood.dbsibycgf.calender;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import in.collectivegood.dbsibycgf.R;
 
 public class CalendarActivity extends AppCompatActivity {
-
+CalendarView calendarView;
+    TextView T;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        calendarView=(CalendarView)findViewById(R.id.calenderview);
+        T=(TextView)findViewById(R.id.Event);
+        calendarView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                T.setText("Event Changed"+calendarView.getDate());
+            }
+        });
+
     }
 }
