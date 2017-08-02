61728fc6b158b56aabe6f4d84a79bc9615d2a0a7
sai <sai.bhavanikumae6393@gmail.com>
2017-08-02 11:28:32 +0530

    gis init

---
+++ app/src/main/java/in/collectivegood/dbsibycgf/gis/GISActivity.java
@@ -0,0 +1,15 @@
+package in.collectivegood.dbsibycgf.gis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.collectivegood.dbsibycgf.R;
+
+public class GISActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gis);
+    }
+}
