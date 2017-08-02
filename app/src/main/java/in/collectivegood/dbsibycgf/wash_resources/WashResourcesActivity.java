d6e8841f7462093424e3e7ff220344d72ed7485f
sai <sai.bhavanikumae6393@gmail.com>
2017-08-02 12:42:55 +0530

    washresources

---
+++ app/src/main/java/in/collectivegood/dbsibycgf/wash_resources/WashResourcesActivity.java
@@ -0,0 +1,15 @@
+package in.collectivegood.dbsibycgf.wash_resources;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.collectivegood.dbsibycgf.R;
+
+public class WashResourcesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wash_resources);
+    }
+}
