fe68f7deefdd5352b6390c36e79f7d3243af3f30
sanjit <sanjitschouhan@gmail.com>
2017-08-03 19:46:29 +0530

    Calendar Activity, TODO Update

--- app/src/main/java/in/collectivegood/dbsibycgf/calender/CalendarActivity.java
+++ app/src/main/java/in/collectivegood/dbsibycgf/calender/CalendarActivity.java
@@ -1,5 +1,6 @@
 package in.collectivegood.dbsibycgf.calender;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -7,23 +8,47 @@ import android.widget.CalendarView;
 import android.widget.TextView;
 
 import in.collectivegood.dbsibycgf.R;
+import in.collectivegood.dbsibycgf.database.DbHelper;
+import in.collectivegood.dbsibycgf.database.Schemas;
+import in.collectivegood.dbsibycgf.database.SchoolDbHelper;
+import in.collectivegood.dbsibycgf.profiles.CCProfileActivity;
 
 public class CalendarActivity extends AppCompatActivity {
-CalendarView calendarView;
+    CalendarView calendarView;
     TextView T;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        calendarView=(CalendarView)findViewById(R.id.calenderview);
-        T=(TextView)findViewById(R.id.Eventname);
+        calendarView = (CalendarView) findViewById(R.id.calenderview);
+        T = (TextView) findViewById(R.id.Eventname);
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                T.setText("Event Changed"+year+month);
+                T.setText("Event Changed" + year + month);
             }
         });
 
 
+        /*
+          TODO: Create a CalendarItem class with required data members
+          TODO: Create constructor and getters using Alt+Insert in that class
+          TODO: 3/8/17 Retrieve calendar data from /calendar/<state>/<ccuid> for all cc,
+          TODO:      ccuid can be found by CCProfileActivity.getCcUID()
+          TODO:     state can be retrieved from getCCState() function below
+          TODO: 3/8/17 Retrive calendar data from /calendar/all for all cc
+          */
+
+    }
+
+    private String getCCState() {
+        SchoolDbHelper schoolDbHelper = new SchoolDbHelper(new DbHelper(this));
+        Cursor read = schoolDbHelper.read(Schemas.SchoolDatabaseEntry.UID_OF_CC, CCProfileActivity.getCcUID());
+        read.moveToNext();
+        String state = read.getString(read.getColumnIndexOrThrow(Schemas.SchoolDatabaseEntry.STATE));
+        read.close();
+
+        return state;
     }
 }
