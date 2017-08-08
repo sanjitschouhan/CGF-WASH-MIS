e7218960a1ab6f58f952b9e522f858fe2f301697
sanjit <sanjitschouhan@gmail.com>
2017-08-08 15:33:05 +0530

    GIS

--- app/src/main/java/in/collectivegood/dbsibycgf/checkin/CheckInLoadingActivity.java
+++ app/src/main/java/in/collectivegood/dbsibycgf/checkin/CheckInLoadingActivity.java
@@ -6,29 +6,83 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.lang.ref.Reference;
-
 import in.collectivegood.dbsibycgf.R;
 
 public class CheckInLoadingActivity extends AppCompatActivity {
+    private static final int PERMISSIONS_REQUEST_LOCATION = 1;
     LocationManager locationManager;
     LocationListener locationListener;
+    Location location;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_LOCATION) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_in_loading);
         final String code = getIntent().getExtras().getString("code");
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location loc) {
+                DatabaseReference gis = null;
+                if (code != null) {
+                    Log.e("onLocationChanged: ", "updating gis " + code);
+                    gis = FirebaseDatabase.getInstance().getReference("gis").child(code);
+
+                    gis.setValue(loc).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            CheckIn(code);
+                        }
+                    });
+                }
+            }
+
+
+            @Override
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("gis");
         databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
@@ -36,13 +90,12 @@ public class CheckInLoadingActivity extends AppCompatActivity {
 
                 for (DataSnapshot g : dataSnapshot.getChildren()) {
                     if (g.getKey().equals(code)) {
-                        Checkin(code);
+                        CheckIn(code);
                         finish();
-
                     }
-                    getGis(code);
-
                 }
+                Log.e("onChildAdded: ", "getting gis");
+                getGis();
             }
 
             @Override
@@ -69,48 +122,18 @@ public class CheckInLoadingActivity extends AppCompatActivity {
     }
 
 
-    private void getGis(String code) {
-
-
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-
-
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
+    private void getGis() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    PERMISSIONS_REQUEST_LOCATION);
             return;
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-
-
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2, 1, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2, 1, locationListener);
     }
 
-    private void Checkin(String code) {
+    private void CheckIn(String code) {
+        finish();
     }
 }
